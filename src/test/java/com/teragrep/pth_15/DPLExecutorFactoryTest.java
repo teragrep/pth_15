e8e40b36c7fab79cca29f51d63acf57c59192827
eemhu <125959687+eemhu@users.noreply.github.com>
2025-09-09 09:50:18 +0300

    Initial version (#1)
    * add pom.xml, workflows, license header and formatting rules
    
    * initial interfaces and factory, tests
    
    * add ci and coverity yamls
    
    * explicitly set maven-surefire-plugin version
    
    * fix RAT exclusions
    
    * replace listener with queryProgressConsumer in DPLExecutor interface
    
    * update license header year
    
    * add "for Teragrep" suffix to license headers
    
    * replace list of message log messages with a map of metrics
    
    * add basic readme

---
+++ src/test/java/com/teragrep/pth_15/DPLExecutorFactoryTest.java
@@ -0,0 +1,74 @@
+/*
+ * pth_15 DPL translator interface library for Teragrep
+ * Copyright (C) 2025 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.pth_15;
+
+import com.typesafe.config.ConfigFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public final class DPLExecutorFactoryTest {
+
+    @Test
+    public void testFactorySuccess() {
+        final DPLExecutorFactory factory = new DPLExecutorFactory(
+                "com.teragrep.pth_15.DPLExecutorTestImpl",
+                ConfigFactory.empty()
+        );
+
+        final DPLExecutor executor = Assertions.assertDoesNotThrow(factory::create);
+
+        Assertions.assertEquals(DPLExecutorTestImpl.class, executor.getClass());
+    }
+
+    @Test
+    public void testFactoryFailure() {
+        final DPLExecutorFactory factory = new DPLExecutorFactory(
+                "com.teragrep.pth_15.TestFailure",
+                ConfigFactory.empty()
+        );
+        Assertions.assertThrows(ClassNotFoundException.class, factory::create);
+    }
+}
